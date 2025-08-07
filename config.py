e177221bce4c770f7fd23f2a836c4eb45ede6d52
Sashas240 <ruslanbortnik70@gmail.com>
2025-08-07 16:23:08 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from enum import Enum
+
+class UserState(Enum):
+    IDLE = "idle"
+    WAITING_FOR_PHONE = "waiting_for_phone"
+    WAITING_IN_QUEUE = "waiting_in_queue"
+    WAITING_FOR_PHOTO = "waiting_for_photo"
+    WAITING_FOR_SUPPORT_MESSAGE = "waiting_for_support_message"
+    WAITING_FOR_ADMIN_REPLY = "waiting_for_admin_reply"
+
+def load_environment():
+    """Загрузка переменных окружения"""
+    load_dotenv()
+    admin_ids = os.getenv('ADMIN_IDS', '').split(',')
+    admin_ids = [int(admin_id) for admin_id in admin_ids if admin_id.strip().isdigit()]
+    return {
+        'BOT_TOKEN': os.getenv('BOT_TOKEN'),
+        'ADMIN_IDS': admin_ids,
+        'GROUP_LINK': "https://t.me/+0KppidSPsRFmYmUx"
+    }
